20812f47ec5088112b7baab06893d165d1c4e256
veronicafradique <23500543+veronicafradique@users.noreply.github.com>
2017-11-22 13:23:28 +0000

    Add files via upload

---
+++ LTW-FEUP/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to To Do Lists </title>
+  <link rel = "stylesheet" type = "text/css" href = "assets/style.css">
+</head>
+<body>
+  <h1>Please Login or Register</h1>
+  <a href = "login.php">Login</a> or
+  <a href = "register.php">Register</a>
+</body>
+</html>
