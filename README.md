d2d59cba4e551e7f73f48c7db653af06d0fe0dd6
David Falcão <davidrsfalcao@gmail.com>
2017-11-25 15:35:03 +0000

    fix login after register

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # LTW-FEUP
+
+**Ligar VPN**
+<br>
+[Ver](http://gnomo.fe.up.pt/~up201506571/projeto/)
+<br>
+<br>
+**Base de dados atual (para testes)**
+<br>
+<br>
+  username  | first name | last name  | password
+:----------:|:----------:|:----------:|:----------:|
+root        | Root       |  Master    | root
--- action_register.php
+++ action_register.php
@@ -1,15 +1,22 @@
 <?php
-
 include_once('config/init.php');
 include_once('database/user.php');
-
 
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $username = trim(strip_tags($_POST['username']));
 $password = $_POST['password'];
 
-createUser($first_name, $last_name, $username, $password);
 
-header('Location: index.php');
+if (userExists($username)){
+    // acrescentar flag erros
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+}
+else {
+    createUser($first_name, $last_name, $username, $password);
+    $_SESSION['username'] = $username;
+    $_SESSION['first_name'] = get_user_first_name($username);
+    $_SESSION['last_name'] = get_user_last_name($username);
+    header('Location: index.php');
+}
 ?>
