b6009d09591428a91de4d7a3b0fa7aafe4c218fe
David Falcão <davidrsfalcao@gmail.com>
2017-12-05 01:00:13 +0000

    grid view

--- templates/main_page.php
+++ templates/main_page.php
@@ -8,30 +8,43 @@
     </div>
 </header>
 
-<div class="question_box">
-    <p>Do you ever forget to do something important </p>
-    <span id="question" class="icon question"></span>
-
-</div>
-
-<div class="apresentation_box">
-    <div class="box" id="column1">
-        <img class="img-responsive center-block" src="images/templates/people.jpg" alt="">
+<div class="main_grid">
+    <div class="question_box">
+        <p>Do you ever forget to do something important </p>
+        <span id="question" class="icon question"></span>
     </div>
-    <div class="box" id="column2">
+
+    <div class="people_box">
+        <img src="images/templates/people.jpg" alt="">
+    </div>
+
+    <div class="prom_box">
         <h2>Don't worry, you're not the only one!</h2>
         <p>People forget things, it's a fact,
-        no one is immune to that!
-        <br>
-        Age, stress, busy life ...  are only a few causes of forgetfulness.
-        But, for many causes, we have only 1 solution :
-        <br>
-        <br>
-        <h1> Easy Notes </h1>
+            no one is immune to that! Age, stress,
+            busy life ...  are only a few causes of forgetfulness.
+            But, for many causes, we have only 1 solution :
+            <br>
+            <br>
+            <h1> Easy Notes </h1>
         </p>
     </div>
+
+
 </div>
 
+<!-- <div class="prom_box">
+<h2>Don't worry, you're not the only one!</h2>
+<p>People forget things, it's a fact,
+no one is immune to that!
+<br>
+Age, stress, busy life ...  are only a few causes of forgetfulness.
+But, for many causes, we have only 1 solution :
+<br>
+<br>
+<h1> Easy Notes </h1>
+</p>
+</div> -->
 
 <!-- <section class="content content-2">
 <div class="container">
