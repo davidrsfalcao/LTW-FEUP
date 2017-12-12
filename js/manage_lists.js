7e8b95e0789fbdcd9bb81564a8d35f98cd272d52
David Falcão <davidrsfalcao@gmail.com>
2017-12-12 01:32:25 +0000

    delete list (prototype)

--- js/manage_lists.js
+++ js/manage_lists.js
@@ -84,8 +84,16 @@ function zoom(html){
         box.style.color = colors[0].color;
     }
     else {
+        let trash = '<form>'
+        +'<span class="trash_icon" onclick="deleteList()" >'
+        +'<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">'
+        +'<title>bin</title>'
+        +'<path d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>'
+        +'<path d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>'
+        +'</svg></span>'
+        +'</form>';
 
-        box.innerHTML = showFullDetails(id);
+        box.innerHTML = showFullDetails(id) + trash;
         let cc = (id % (colors.length -1)) + 1;
         box.style.background = colors[cc].background;
         box.style.color = colors[cc].color;
@@ -94,7 +102,6 @@ function zoom(html){
     box.className='zoom_box';
     box.setAttribute('onmousemove', 'mouseIsOverZoomBox(true)');
     box.setAttribute('onmouseleave', 'mouseIsOverZoomBox(false)');
-
     div.appendChild(box);
     document.body.appendChild(div);
     if(id == 0){
@@ -320,7 +327,6 @@ function addItem(value){
         default:
         break;
     }
-
     document.getElementById('zoom_box_item').innerHTML = content;
     return content;
 }
@@ -598,8 +604,6 @@ function updateGridLists(){
         document.getElementById('display_lists_grid').appendChild(div);
     }
 
-    console.log("HERE");
-
 }
 
 function showDetails(elem){
@@ -775,4 +779,11 @@ function refreshGrid(){
 
 function setToken(token1){
     token = token1;
+}
+
+function deleteList(){
+
+    confirm('Are you sure you want to delete this list?'
+    + '\nThis is irreversible');
+
 }
